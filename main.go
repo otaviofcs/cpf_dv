29f6cdd95e3c0b41a47a66a8599a1788ef991e3b
Otavio Sampaio <otaviofcs@gmail.com>
2020-11-19 09:27:58 -0300

    formatting output as json. Need improvement and tests....

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "fmt"
   "log"
   "net/http"
+  "encoding/json"
   "os"
   cpf "github.com/otaviofcs/cpf_dv/cpf"
 )
@@ -11,7 +11,13 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
   var baseCpf string = r.URL.Path[len("/cpfdv/"):]
   cpfdv, _ := cpf.CpfComDV(baseCpf)
-  fmt.Fprintf(w, "Hi, complete CPF is: '%s'", cpfdv)
+  v := map[string] interface{}{
+    "cpf": cpfdv,
+  }
+  enc := json.NewEncoder(w)
+  if err := enc.Encode(&v); err != nil {
+    log.Println(err)
+  }
 }
 
 func main() {
