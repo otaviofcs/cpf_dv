d84ffecd55218d7bfb82446ffdd570a357f919bf
Otavio Sampaio <otaviofcs@gmail.com>
2020-11-25 22:35:19 -0300

    server.go needs to move to its own folder

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ import (
 )
 /*
  * Environment Variables:
- * ENTRY_POINT = "/cpfdv/"
  * PORT = 8080
  *
  * Using Google Functions standard env entries:
  * https://cloud.google.com/functions/docs/env-var
  */
 func main() {
-    http.HandleFunc(os.Getenv("ENTRY_POINT"), CpfFull)
-    log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+  entry_point := "/cpf_dv/"
+  os.Setenv("ENTRY_POINT", entry_point)
+  http.HandleFunc(entry_point, CpfFull)
+  log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
